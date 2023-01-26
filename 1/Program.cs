04d113d4a36d0a186a16dd2c7a0a58ce461ddc5d
Denis <damned_wolf@mail.ru>
2023-01-26 21:14:31 +0300

    Задача 2

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+if (numberTwo*numberTwo == numberOne){
+    Console.WriteLine($"Число {numberOne} является квадратом {numberTwo}");
+}
+else {
+    Console.WriteLine($"Число {numberOne} не является квадратом {numberTwo}");
+}
