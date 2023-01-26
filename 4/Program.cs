1d7d461654fd64c09b1f362df1056358e554a104
Denis <damned_wolf@mail.ru>
2023-01-26 22:14:09 +0300

    Задача 5

---
+++ 4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 999 || number < -999)
+{
+    Console.WriteLine("Вы ввели некоректное число");
+    return;
+}
+else if (number > 0)
+{
+    Console.WriteLine(number % 10);
+}
+else if (number < 0)
+{
+    Console.WriteLine(number % 10 * -1);
+}
