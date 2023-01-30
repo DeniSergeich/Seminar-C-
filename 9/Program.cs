e131d4143a7fea07425514f21f8e3b582a8f8df0
Denis <damned_wolf@mail.ru>
2023-01-30 23:04:21 +0300

    Задача 9

---
+++ 9/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
