941c38c0275d60d11d78ed08df7741ee04701bd6
Denis <damned_wolf@mail.ru>
2023-02-02 21:15:52 +0300

    Задача 13

---
+++ 13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите xA: ");
+double xA = double.Parse(Console.ReadLine());
+Console.Write("Введите yA: ");
+double yA = double.Parse(Console.ReadLine());
+Console.Write("Введите xB: ");
+double xB = double.Parse(Console.ReadLine());
+Console.Write("Введите yB: ");
+double yB = double.Parse(Console.ReadLine());
+double l = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2));
+Console.WriteLine($"Расстояние между точками равно: {l:f2}");
