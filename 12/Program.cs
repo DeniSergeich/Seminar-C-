dc2eca81150e96d73ca0716d6802203d58193ca8
Denis <damned_wolf@mail.ru>
2023-02-02 20:35:56 +0300

    Задача 12

---
+++ 12/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти координат: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 1 || number > 4)
+{
+    Console.WriteLine("Введите число от 1 до 4!");
+    return;
+}
+if (number == 1)
+{
+    Console.WriteLine("Диапазон координат: (X;Y)");
+}
+if (number == 2)
+{
+    Console.WriteLine("Диапазон координат: (-X;Y)");
+}
+if (number == 3)
+{
+    Console.WriteLine("Диапазон координат: (-X;-Y)");
+}
+if (number == 4)
+{
+    Console.WriteLine("Диапазон координат: (X;-Y)");
+}
