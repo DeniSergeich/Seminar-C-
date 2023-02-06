7039c766a56bbb906620778f93694b3102145702
Denis <damned_wolf@mail.ru>
2023-02-06 23:38:49 +0300

    Задача 15

---
+++ 15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
+int GetSum(int limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)
+    {
+        sum += i;
+
+    }
+    return sum;
+}
