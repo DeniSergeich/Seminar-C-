d67ea87cb6d714cff6ca65578ca91fba0056a5b5
Denis <damned_wolf@mail.ru>
2023-02-06 23:39:08 +0300

    Задача 17

---
+++ 17/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int MultNum(int number)
+{
+    int prod = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        prod *= i;
+    }
+    return prod;
+}
+Console.WriteLine($"Произведение чисел от 1 до {number}: {MultNum(number)}");
