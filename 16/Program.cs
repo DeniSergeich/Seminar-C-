b6860cf49085240ff36fdfa8cca44f2e23b664a8
Denis <damned_wolf@mail.ru>
2023-02-06 23:38:57 +0300

    Задача 16

---
+++ 16/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int GetNum(int number)
+{
+    int count = 0;
+    if (number < 0)
+    {
+        number *= -1;
+    }
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine($"Количество цифр в числе: {GetNum(number)}");
