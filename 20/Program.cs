dd67544f5c11979d4ae6e4b42384c2c737f87a17
Denis <damned_wolf@mail.ru>
2023-02-13 19:53:21 +0300

    Task 20

---
+++ 20/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int[] array = GetRandomArray(8, -10, 10);
+Console.WriteLine($"Изначально: {String.Join(" ", array)}");
+Console.WriteLine($"После преобразования: {String.Join(" ", InverseArray(array))}");
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+int[] InverseArray(int[] array)
+{ 
+     
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
