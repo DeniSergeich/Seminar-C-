d79702e938e3fa41abebe56f4023112e1e648a33
Denis <damned_wolf@mail.ru>
2023-02-13 22:16:25 +0300

    Task 24

---
+++ 24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Перевод из десятичной системы в 2...16
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string res = DecToNumber(number, 3);
+Console.WriteLine($"{number} -> {res}");
+
+string DecToNumber(int decNumber, int otherSystem)
+{
+    string res = "";
+    string nums = "0123456789abcdef";
+    while(decNumber>0){
+        int ost = decNumber/otherSystem;
+        res = nums[decNumber-otherSystem*ost]+res;
+        decNumber/= otherSystem;
+    }
+    return res;
+}
