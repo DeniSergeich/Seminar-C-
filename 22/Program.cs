43266cfca48d13aa2e523ae13b53d6a9229e004b
Denis <damned_wolf@mail.ru>
2023-02-13 22:16:08 +0300

    Task 22

---
+++ 22/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+int[] array = GetRandomArray(10, 0, 10);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] ReverseArray(int[] array)
+{
+    int[] revArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        revArray[i] = array[array.Length - 1 - i];
+    }
+    return revArray;
+}
+Console.WriteLine($"[{String.Join(", ", ReverseArray(array))}]");
