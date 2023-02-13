d1ccb45b6e268826ec2b1af559a87c1cd173a267
Denis <damned_wolf@mail.ru>
2023-02-13 22:16:34 +0300

    Task 25

---
+++ 25/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int[] array = GetRandomArray(10,0,10);
+Console.WriteLine($"Array: {String.Join(' ', array)}");
+Console.WriteLine($"Copy Array: {String.Join(' ', copyArray(array))}");
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+int[] copyArray(int[]array){
+    int[] copyArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
