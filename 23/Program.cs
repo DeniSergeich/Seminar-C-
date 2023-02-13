fa06c381cabd260c273a5140829467b67970fd47
Denis <damned_wolf@mail.ru>
2023-02-13 22:16:17 +0300

    Task 23

---
+++ 23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите первую сторону: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите вторую сторону: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третью сторону: ");
+int c = int.Parse(Console.ReadLine());
+if (IsTriangle(a, b, c))
+{
+    Console.WriteLine("Может!");
+}
+else
+{
+    Console.WriteLine("Не может!");
+}
+bool IsTriangle(int a, int b, int c)
+{
+    return a + b > c && a + c > b && c + b > a;
+}
