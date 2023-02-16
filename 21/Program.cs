a40a7d8b5ad0d4b6ab29cbd87ee80ea665d35927
Denis <damned_wolf@mail.ru>
2023-02-16 19:42:31 +0300

    Task 21

---
+++ 21/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = GetRandomArray(8, -10, 10);
+Console.WriteLine(String.Join(" ", array));
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+string Res(int[] array, int n)
+{
+    string resultSTR = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == n)
+        {
+            resultSTR = "Yes";
+            return resultSTR;            
+        }
+        else
+        {
+            resultSTR = "No";
+        }
+    }
+    return resultSTR;
+}
+Console.WriteLine(Res(array, n));
