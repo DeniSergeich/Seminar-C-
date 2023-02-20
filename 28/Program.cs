d2d534bad37bf3e86f4f53e09ced0e0041abd222
Denis <damned_wolf@mail.ru>
2023-02-20 19:54:51 +0300

    Task 28

---
+++ 28/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int m = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int n = int.Parse(ReadLine());
+
+int[,] array = GetArray(m, n, 0, 10);
+PrintArray(array);
+WriteLine();
+PrintArray(GetModArray(array));
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+int[,] GetModArray(int[,] array)
+{
+    int[,] modArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 != 0 && j % 2 != 0)
+            {
+                modArray[i, j] = array[i, j] * array[i, j];
+            }
+            else
+            {
+                modArray[i, j] = array[i, j];
+            }
+        }
+    }
+    return modArray;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
