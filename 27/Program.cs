0c0ef3e27a46d8a47324c04ac05d09550acf3fae
Denis <damned_wolf@mail.ru>
2023-02-20 19:54:44 +0300

    Task 27

---
+++ 27/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int m = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int n = int.Parse(ReadLine());
+
+int[,] array = GetArray(m, n);
+PrintArray(array);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i+j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
