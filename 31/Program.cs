a6e7f125153a21931196e4f787a1e1beab921c16
Denis <damned_wolf@mail.ru>
2023-02-20 23:08:55 +0300

    Task 31

---
+++ 31/Program.cs
@@ -0,0 +1,58 @@
+﻿// Замена строк массива на столбцы (строка переходит в столбец в новом массиве)
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int m = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int n = int.Parse(ReadLine());
+
+
+int[,] array = FillArray(m, n, 0, 9);
+PrintArray(array);
+int[,] resultArray = GetArray(array);
+WriteLine();
+PrintArray(resultArray);
+
+int[,] FillArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] GetArray(int[,] array)
+{
+    int[,] resultArray = new int[array.GetLength(1), array.GetLength(0)];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            resultArray[j,i] = array[i,j];
+        }
+    }
+    return resultArray;
+}
