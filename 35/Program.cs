da6cf9e6d3cb75d4490704aca90f74f612482430
Denis <damned_wolf@mail.ru>
2023-02-23 22:30:43 +0300

    Task 35

---
+++ 35/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+
+// 453 -> 12
+
+// 45 -> 9
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+WriteLine(SumNumber(number));
+
+int SumNumber(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return (number % 10 + SumNumber(number / 10));
+}
